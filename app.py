eeb7ca9109c83e66cfb68a96b365286f050b2a33
u3533791 <61692097+u3533791@users.noreply.github.com>
2020-09-05 02:39:28 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+import requests
+import re
+
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()
+    url = contents['url']
+    return url
+
+def bop(bot, update):
+    url = get_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+
+def main():
+    updater = Updater('1061725774:AAGbDCbX10kxU2dhkstTfKLJV6vgLNsN2fs', use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('bop',bop))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
